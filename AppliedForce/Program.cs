ebbd5a8836bd8aaea0dbfd4ca75092839c1475f9
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-19 20:57:01 -0600

    Agregar archivos de proyecto.

---
+++ AppliedForce/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter the mass (in kilograms): ");
+        double mass = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the acceleration (in meters per second squared): ");
+        double aceleration = Convert.ToDouble(Console.ReadLine());
+
+        //fórmula F = ma
+        double force = mass * aceleration;
+
+        
+        Console.WriteLine($"The applied force is: {force} newtons");
+    }
+}
